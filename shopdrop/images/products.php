ea5b584227467170c2563d47b2040ae312f9a258
Dhanushree879 <55574653+Dhanushree879@users.noreply.github.com>
2020-05-31 07:41:25 +0530

    Add files via upload

---
+++ shopdrop/images/products.php
@@ -0,0 +1,307 @@
+<?php
+    session_start();
+    require 'check_if_added.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>product page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">SHOPDROP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#footer">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cart.php">Cart</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">LOGOUT</a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+</head>
+<body>
+  <section class="single-product">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5">
+          <div id="carouselExample" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="watch1.webp" class="d-block" style="width: 30; height: 20" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="w2.webp" class="d-block " style="width: 30; height: 20" alt="...">
+    </div>
+    <!--<div class="carousel-item">
+      <img src="watch3.png" class="d-block" style="width: 30; height: 20" alt="...">
+    </div>--><a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+</div>
+</div>
+<div class="col-md-7">
+  <p class="new-arrival">NEW</p>
+  <h2>Black Dial Silver Stainless Steel Strap Watch</h2>
+  <p>Product Code: NL2480SM02</p>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <i class="fa fa-star-half-o"></i>
+          <p class="price">USD $15.00</p>
+          <p><b>Availability</b> In Stock</p>
+          <p><b>Condition:</b> New</p>
+          <p><b>Brand:</b> TITAN Company</p>
+          <label>Quantity</label>
+          <input type="text" value="1">
+         <!--<a href="cart.php"> <button type="button" class="btn btn-primary">Add to Cart</button></a>-->
+ <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(1)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=1" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+</div>
+</div>
+</div>
+</section>
+<section class="product-description">
+  <div class="container">
+    <h6>Product Description</h6>
+    <p>This analog watch for men features a black, round dial, encompassed within a stainless steel case that is fitted with a crown. The metallic strap is black in colour. The dial features plain two hands, three subdials, a date indicator, and Hindu Arabic numerals at quarterly intervals. Equipped with 5 ATM water-resistant depth, this watch is user-friendly and it comes with a 24 month warranty.</p>
+    <hr>
+  </div>
+  
+    <div class="container">
+      <div class="title-box">
+        <h2>Similar</h2>
+      </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="product-top">
+          <img src="fw.jpg">
+          <div class="overlay-right">
+            <button type="button" class="btn btn-secondary" title="Quick Shop">
+              <i class="fa fa-eye"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="Add to wishlist">
+              <i class="fa fa-heart-o"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="add to cart">
+              <a href="product1.html"><i class="fa fa-shopping-cart"></i></a>
+            </button>
+          </div>
+        </div>
+        <div class="product-bottom text-center">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <h3>Fitness Watch</h3>
+          <h5>$40.00</h5>
+          
+
+      </div>
+    </div>
+    <div class="col-md-3">
+        <div class="product-top">
+          <img src="purse.jpg">
+          <div class="overlay-right">
+            <button type="button" class="btn btn-secondary" title="Quick Shop">
+              <i class="fa fa-eye"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="Add to wishlist">
+              <i class="fa fa-heart-o"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="add to cart">
+              <a href="product1.html"><i class="fa fa-shopping-cart"></i></a>
+            </button>
+          </div>
+        </div>
+        <div class="product-bottom text-center">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <h3>Woodland Wallet</h3>
+          <h5>$50.00</h5>
+          
+
+      </div>
+    </div>
+    <div class="col-md-3">
+        <div class="product-top">
+          <img src="slippers.jpg">
+          <div class="overlay-right">
+            <button type="button" class="btn btn-secondary" title="Quick Shop">
+              <i class="fa fa-eye"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="Add to wishlist">
+              <i class="fa fa-heart-o"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="add to cart">
+              <a href="product1.html"><i class="fa fa-shopping-cart"></i></a>
+            </button>
+          </div>
+        </div>
+        <div class="product-bottom text-center">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <i class="fa fa-star-half-o"></i>
+          <h3>Reforce Slippers</h3>
+          <h5>$30.00</h5>
+          
+
+      </div>
+    </div>
+    <div class="col-md-3">
+        <div class="product-top">
+          <img src="ags.jpg">
+          <div class="overlay-right">
+            <button type="button" class="btn btn-secondary" title="Quick Shop">
+              <i class="fa fa-eye"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="Add to wishlist">
+              <i class="fa fa-heart-o"></i>
+            </button>
+            <button type="button" class="btn btn-secondary" title="add to cart">
+              <a href="product1.html"><i class="fa fa-shopping-cart"></i></a>
+            </button>
+          </div>
+        </div>
+        <div class="product-bottom text-center">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <h3>Kingson Bags</h3>
+          <h5>$40.00</h5>
+          
+
+      </div>
+    </div>
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <section id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h1>Useful Links</h1>
+        <p>Privacy Policy</p>
+        <p>Terms of Use</p>
+        <p>Return Policy</p>
+        <p>Discount Coupons</p>
+      </div>
+      <div class="col-md-3">
+        <h1>Company</h1>
+        <p>About us</p>
+        <p>Contact Us</p>
+        <p>Affiliate</p>
+        
+      </div>
+      <div class="col-md-3">
+        <h1>Follow us on</h1>
+        <a href="https://www.facebook.com/dhanu.shree.94849"><i class="fa fa-facebook-official"></i></a>
+        <a href="https://www.youtube.com/channel/UCCNbpK_2j3_aWDj7fd2I-eQ?view_as=subscriber"><i class="fa fa-youtube-play"></i></a>
+        <a href="https://www.linkedin.com/in/dhanushree-s-a765a5196/"><i class="fa fa-linkedin"></i></a>
+        <a href="https://twitter.com/s_dhanushree"><i class="fa fa-twitter"></i></a>
+      </div>
+        <div class="col-md-3 footer-image">
+          <h1>Download App</h1>
+          <img src="googlepay.jpg">
+        
+        
+      </div>
+    </div>   <hr>
+  <p class="copyright"> made with <i class="fa fa-heart-o"></i></p>
+
+  </div>
+
+  
+</section>
+<script src="js/smooth-scroll.js"></script>
+<script>
+  var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+        
+      </div>
+        
+    </div>
+  </div>
+  
+</section>
+
+      
+
+
+
+
+
+
+    
+  
+
+</body>
+</html>
+	
+
+  
